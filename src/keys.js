fcdb35e565fa9d32024323f9e6d003f00292a5b9
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-04 00:44:05 +0200

    search page updated

--- src/keys.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY="AIzaSyDlN6aRUJfl_clamNfcKvRDY6mwGGWfGo0";
-
-export default API_KEY;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { useStateValue } from '../datalayer/StateProvider';
-import "./SearchPage.css"
-
+import React from "react";
+import { useStateValue } from "../datalayer/StateProvider";
+import useGoogleSearch from "./useGoogleSearch";
+import "./SearchPage.css";
 
 const SearchPage = () => {
-    const [{ term }, dispatch] = useStateValue();
+  const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
 
-    return (
-        <div className="searchPage">
-            <div className="searchPage__header">
-            <h1>{ term }</h1>
-            </div>
-            <div className="searchPage__results">
+  console.log(data);
+  
+  return (
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results"></div>
+    </div>
+  );
+};
 
-            </div>
-        </div>
-    )
-}
-
-export default SearchPage
+export default SearchPage;
