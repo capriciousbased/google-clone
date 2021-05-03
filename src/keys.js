955667f20abf9e97b5f6358523cf19db531d5cdc
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-03 23:50:31 +0200

    api key and custome hook added

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY="AIzaSyDlN6aRUJfl_clamNfcKvRDY6mwGGWfGo0";
+
+export default API_KEY;
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import API_KEY from "./keys"
+
+const CONTEXT_KEY ="50824759fb8a984e9"
+
+const useGoogleSearch = () => {
+    
+}
+
+export default useGoogleSearch
