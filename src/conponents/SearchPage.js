101228783c7052264a8872f8b35d6b7724aaae47
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-09-24 13:27:43 +0200

    useGoogleSearch custom hook added.

--- src/conponents/SearchPage.js
+++ src/conponents/SearchPage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useStateValue } from "../datalayer/StateProvider";
+import useGoogleSearch from "./useGoogleSearch";
 
 const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
+  console.log(data);
   return (
     <div className="searchPage">
       <div className="searchPage_header">
