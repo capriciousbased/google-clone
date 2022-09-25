445ccb3b983849891858fa7c2d322593bfaabbb8
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-09-25 17:38:08 +0200

    Searchpage option added

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -6,6 +6,13 @@ import useGoogleSearch from "../useGoogleSearch";
 import { Link } from "@material-ui/core";
 import Search from "../components/Search";
 import response from "../response";
+
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue();
   //live apicall
@@ -14,7 +21,7 @@ const SearchPage = () => {
   console.log(data);
   return (
     <div className="searchPage">
-      <div className="searchPage_header">
+      <div class="searchPage__header">
         <Link to="/">
           <img
             className="searchPage__logo"
@@ -22,9 +29,45 @@ const SearchPage = () => {
             alt=""
           />
         </Link>
-      </div>
-      <div className="searchPage_resultsBody">
-        <Search hideButtons />
+        <div class="searchPage__headerBody">
+          <Search hideButtons />
+          <div class="searchPage__options">
+            <div class="searchPage__optionsLeft">
+              <div class="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div class="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/all">News</Link>
+              </div>
+              <div class="searchPage__option">
+                <ImageIcon />
+                <Link to="/all">Images</Link>
+              </div>
+              <div class="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/all">shopping</Link>
+              </div>
+              <div class="searchPage__option">
+                <RoomIcon />
+                <Link to="/all">maps</Link>
+              </div>
+              <div class="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/all">more</Link>
+              </div>
+            </div>
+            <div class="searchPage__optionsRight">
+              <div class="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div class="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
