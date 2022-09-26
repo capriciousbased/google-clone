2044b68a40e47382a1c898ea86d70a8025607bd6
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-09-26 21:09:11 +0200

    search page option and css added

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./SearchPage.css";
-import { useStateValue } from "../datalayer/StateProvider";
+import { useStateProviderValue } from "../datalayer/StateProvider";
 import useGoogleSearch from "../useGoogleSearch";
-
-import { Link } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import Search from "../components/Search";
-import response from "../response";
+import Response from "../response.js";
 
 import SearchIcon from "@material-ui/icons/Search";
 import DescriptionIcon from "@material-ui/icons/Description";
@@ -13,12 +12,18 @@ import ImageIcon from "@material-ui/icons/Image";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import RoomIcon from "@material-ui/icons/Room";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-const SearchPage = () => {
-  const [{ term }, dispatch] = useStateValue();
-  //live apicall
-  //const { data } = useGoogleSearch(term);
-  const data = response;
+
+function SearchPage() {
+  const [{ term }] = useStateProviderValue();
+
+  // Live API Call
+  const { data } = useGoogleSearch(term);
+
+  // Mock API
+  // const data = Response;
+
   console.log(data);
+
   return (
     <div className="searchPage">
       <div class="searchPage__header">
@@ -69,8 +74,38 @@ const SearchPage = () => {
           </div>
         </div>
       </div>
+
+      {term && (
+        <div class="searchPage__results">
+          <p className="searchPage__resultCount">
+            About {data?.searchInformation.formattedTotalResults} results (
+            {data?.searchInformation.formattedSearchTime}
+            seconds) for {term}
+          </p>
+
+          {data?.items.map((item) => (
+            <div className="searchPage__result">
+              <a className="searchPage__resultLink" href={item.link}>
+                {item.pagemap?.cse_image?.length > 0 &&
+                  item.pagemap?.cse_image[0]?.src && (
+                    <img
+                      className="searchPage__resultImage"
+                      src={item.pagemap?.cse_image[0]?.src}
+                      alt=""
+                    />
+                  )}
+                {item.displayLink} ∆
+              </a>
+              <a href={item.link} className="searchPage__resultTitle">
+                <h2>{item.title}</h2>
+              </a>
+              <p className="searchPage__resultSnippet">{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default SearchPage;
